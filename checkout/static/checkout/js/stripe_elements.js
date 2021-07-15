5c926e488040dd7e7f80b8f4574be91b1423f564
Mjau83 <mamoni.hemlin@gmail.com>
2021-07-15 17:58:42 +0000

    Comleted webhook handler

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -110,7 +110,7 @@ form.addEventListener('submit', function(ev) {
                 }
             }
         });
-    }).fail(function () {
+    }).fail(function() {
         // just reload the page, the error will be in django messages
         location.reload();
     })
